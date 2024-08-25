0312e6a9315c708da77161f50c9d519573831bc6
Somgester <dhruvratmele13@gmail.com>
2024-08-25 15:56:16 +0530

    added some more weather types

--- script.js
+++ script.js
@@ -15,32 +15,33 @@ document.getElementById('submit').addEventListener('click', function () {
             document.getElementById('temp').innerHTML = Math.floor(data.main.temp) + "°C";
             document.getElementById('type').innerHTML = data.weather[0].main;
 
-            if (type.innerHTML == "Clouds")
-            {
+            if (type.innerHTML == "Clouds") {
                 image.src = "clouds.png";
             }
-            else if (type.innerHTML == "Clear") 
-            {
+            else if (type.innerHTML == "Clear") {
                 image.src = "clearsky.png";
             }
-            else if (type.innerHTML == "Rain") 
-            {
+            else if (type.innerHTML == "Rain") {
                 image.src = "rain.webp";
             }
-            else if (type.innerHTML == "Mist" || type.innerHTML == "Haze") 
-            {
+            else if (type.innerHTML == "Mist" || type.innerHTML == "Haze") {
                 image.src = "haze.png";
             }
-            else if (type.innerHTML == "Smoke") 
-            {
+            else if (type.innerHTML == "Smoke") {
                 image.src = "fog.avif";
             }
-            else if (type.innerHTML == "Snow") 
-            {
+            else if (type.innerHTML == "Snow") {
                 image.src = "snow.png";
             }
+            else if (type.innerHTML == "Thunderstorm") {
+                image.src = "thunderstorm.png";
+            }
+            else if (type.innerHTML == "Drizzle") {
+                image.src = "drizzle.png";
+            }
 
-            
+
+
         }
         )
         .catch(error => console.error('Error:', error));
@@ -51,45 +52,45 @@ function handleEnter(event) {
         let image = document.getElementById('image');
 
         let cityName = document.getElementById('search-box').value;
-    
+
         let API_key = "c995eadaef9f14a9fdf4956d5f570c38";
-    
+
         let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_key}&units=metric`;
-    
+
         fetch(url)
             .then(response => response.json())
             .then(data => {
-    
+
                 document.getElementById('city').innerHTML = data.name;
                 document.getElementById('temp').innerHTML = Math.floor(data.main.temp) + "°C";
                 document.getElementById('type').innerHTML = data.weather[0].main;
-    
-                if (type.innerHTML == "Clouds")
-                {
+
+                if (type.innerHTML == "Clouds") {
                     image.src = "clouds.png";
                 }
-                else if (type.innerHTML == "Clear") 
-                {
+                else if (type.innerHTML == "Clear") {
                     image.src = "clearsky.png";
                 }
-                else if (type.innerHTML == "Rain") 
-                {
+                else if (type.innerHTML == "Rain") {
                     image.src = "rain.webp";
                 }
-                else if (type.innerHTML == "Mist" || type.innerHTML == "Haze") 
-                {
+                else if (type.innerHTML == "Mist" || type.innerHTML == "Haze") {
                     image.src = "haze.png";
                 }
-                else if (type.innerHTML == "Smoke") 
-                {
+                else if (type.innerHTML == "Smoke") {
                     image.src = "fog.avif";
                 }
-                else if (type.innerHTML == "Snow") 
-                {
+                else if (type.innerHTML == "Snow") {
                     image.src = "snow.png";
                 }
-    
-                
+                else if (type.innerHTML == "Thunderstorm") {
+                    image.src = "thunderstorm.png";
+                }
+                else if (type.innerHTML == "Drizzle") {
+                    image.src = "drizzle.png";
+                }
+
+
             }
             )
             .catch(error => console.error('Error:', error));
